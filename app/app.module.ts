561b275a921c3c02d78a36a9e56c4b69a6485e30
elvis <mosoti17@gmail.com>
2017-07-05 11:49:35 +0300

    added adding new task

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { PiesListComponent } from './pies-list.component';
 import { AppComponent }   from './app.component';
-
+import { TaskListComponent } from './tasks-list.component';
+import { EditTaskComponent } from './edit-task.component';
+import { NewTaskComponent } from './new-task.component';
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [ AppComponent ],
+  imports: [BrowserModule,FormsModule ],
+  declarations: [AppComponent,PiesListComponent,TaskListComponent,EditTaskComponent,NewTaskComponent],
   bootstrap:    [ AppComponent ]
 })
 
